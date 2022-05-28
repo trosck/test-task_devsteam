0ff3d15accbafe33e2725f589e0baac4a79e49b3
Sergey Belov <ser.belov00@yandex.ru>
2022-05-28 10:07:32 +0300

    fix spaces

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -2,5 +2,5 @@ import { combineReducers } from "redux";
 import ImageReducer from "./ImageReducer";
 
 export default combineReducers({
-	image: ImageReducer
+  image: ImageReducer
 })
--- views/ImageScreen.js
+++ views/ImageScreen.js
@@ -1,64 +1,64 @@
 import React, { useState } from "react";
 import {
-	View,
-	Image,
-	Text,
-	StyleSheet
+  View,
+  Image,
+  Text,
+  StyleSheet
 } from "react-native";
 import { connect } from "react-redux";
 
 const ImageScreen = (props) => {
-	const [loading, setLoading] = useState(true);
-	const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
-	return (
-		<View>
-			{loading ? (
-				<View style={styles.loading}>
-					<Text style={styles.loadingText}>Загрузка, подождите...</Text>
-				</View>
-			) : error.length ? (
-				<Text>Произошла ошибка! {error}</Text>
-			) : null}
+  return (
+    <View>
+      {loading ? (
+        <View style={styles.loading}>
+          <Text style={styles.loadingText}>Загрузка, подождите...</Text>
+        </View>
+      ) : error.length ? (
+        <Text>Произошла ошибка! {error}</Text>
+      ) : null}
 
-			<Image
-				onError={error => setError(error)}
+      <Image
+        onError={error => setError(error)}
 
-				onLoadStart={() => setLoading(true)}
-				onLoadEnd={() => setLoading(false)}
+        onLoadStart={() => setLoading(true)}
+        onLoadEnd={() => setLoading(false)}
 
-				style={loading ? null : styles.image}
-				resizeMode="contain"
-				source={{
-					uri: props.image.fullView
-				}}
-			/>
-		</View>
-	);
+        style={loading ? null : styles.image}
+        resizeMode="contain"
+        source={{
+          uri: props.image.fullView
+        }}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-	image: {
-		height: "100%"
-	},
+  image: {
+    height: "100%"
+  },
 
-	loading: {
-		display: "flex",
-		justifyContent: "center",
-		alignItems: "center",
-		height: "100%",
-	},
+  loading: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+  },
 
-	loadingText: {
-		fontSize: 24,
-		fontStyle: "italic"
-	}
+  loadingText: {
+    fontSize: 24,
+    fontStyle: "italic"
+  }
 })
 
 const mapStateToProps = state => {
-	return {
-		image: state.image.currentImage
-	}
+  return {
+    image: state.image.currentImage
+  }
 }
 
 export default connect(mapStateToProps, {})(ImageScreen);
